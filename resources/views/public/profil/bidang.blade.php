bed329d97fa4f87c6db9dfbf546b9cb0ce6f2348
Farma27 <stevenbold60@gmail.com>
2021-05-24 14:39:14 +0700

    24 May

--- resources/views/public/profil/bidang.blade.php
+++ resources/views/public/profil/bidang.blade.php
@@ -32,10 +32,10 @@
                   <th>Nama Pegawai</th><th>Jabatan</th>
                 </tr>
                 <tr>
-                  <td>Table cell 1</td><td>Table cell 2</td>
+                  <td>1</td><td>2</td>
                 </tr>
                 <tr>
-                  <td>Table cell 3</td><td>Table cell 4</td>
+                  <td>3</td><td>4</td>
                 </tr>
               </table>
             </div>
